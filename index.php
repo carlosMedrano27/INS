0db82436e0dd06c1ec3ce33332b996cab32d1e9d
Josué Urquilla <josueurquilla23@outlook.com>
2019-05-06 11:47:21 -0600

    Test

---
+++ index.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>TADESA</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="RanGO Project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
+<link href="plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="plugins/slick-1.8.0/slick.css">
+<link href="plugins/icon-font/styles.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+<link rel="stylesheet" type="text/css" href="styles/responsive.css">
+</head>
+
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+	<header class="header d-flex flex-row justify-content-end align-items-center trans_200">
+		
+		<!-- Logo -->
+		<div class="logo mr-auto">
+			<img src="images/talleres.png" width="100" height="100"> 
+		</div>
+
+		<!-- Navigation -->
+		<nav class="main_nav justify-self-end text-right">
+			<ul>
+				
+				
+				<body>
+				
+				<div class="super_container">
+				<li class="active"><a href="#">Inicio</a></li>
+				<li><a href="about.php">Quiénes somos</a></li>
+				<li><a href="places.php">Instalaciones</a></li>			
+				<li><a href="contributions.php">Donaciones</a></li>
+				<li><a href="contact.php">Contáctanos</a></li>
+			</ul>
+			
+		<!-- Hamburger -->
+		<div class="hamburger_container bez_1">
+			<i class="fas fa-bars trans_200"></i>
+		</div>
+		
+	</header>
+
+	<!-- Menu -->
+
+	<div class="menu_container">
+		<div class="menu menu_mm text-right">
+			<div class="menu_close"><i class="far fa-times-circle trans_200"></i></div>
+			<ul class="menu_mm">
+				<li class="menu_mm active"><a href="#">Inicio</a></li>
+				<li class="menu_mm"><a href="about.php">Quiénes somos</a></li>
+				<li class="menu_mm"><a href="places.php">Instalaciones</a></li>
+				<li class="menu_mm"><a href="contributions.php">Donaciones</a></li>
+				<li class="menu_mm"><a href="contact.php">Contáctanos</a></li>
+			</ul>
+		</div>
+	</div>
+
+	<!-- Home -->
+
+	<div class="home">
+		<div class="home_background_container prlx_parent">
+			<div class="home_background prlx"></div>
+		</div>
+		
+		<!-- Hero Slider -->
+		<div class="hero_slider_container">
+			
+			<!-- Slider -->
+			<div class="owl-carousel owl-theme hero_slider" style="background-image:url(images/slider_background.jpg)">
+
+				<!-- Slider Item -->
+				<div class="owl-item hero_slider_item item_1 d-flex flex-column align-items-center justify-content-center">
+					<span>by</span>
+					<span><img src="images/colorlib.png" alt=""></span>
+					<span>GO!</span>
+					<span>Get your freebie template now!</span>
+					<div><h4>Fecha</h2></div>
+					<div><h1>Titulo</h1></div>
+					<p> Parrafo revisar que quepa todo </p>
+				</div>
+
+				<!-- Slider Item -->
+				<div class="owl-item hero_slider_item item_1 d-flex flex-column align-items-center justify-content-center">
+					<span>by</span>
+					<span><img src="images/colorlib.png" alt=""></span>
+					<span>GO!</span>
+					<span>Get your freebie template now!</span>
+				</div>
+
+				<!-- Slider Item -->
+				<div class="owl-item hero_slider_item item_1 d-flex flex-column align-items-center justify-content-center">
+					<span>by</span>
+					<span><img src="images/colorlib.png" alt=""></span>
+					<span>GO!</span>
+					<span>Get your freebie template now!</span>
+				</div>
+
+			</div>
+			
+			<!-- Hero Slider Navigation Left -->
+			<div class="hero_slider_nav hero_slider_nav_left">
+				<div class="hero_slider_prev d-flex flex-column align-items-center justify-content-center trans_200">
+					<i class="fas fa-chevron-left trans_200"></i>
+				</div>
+			</div>
+
+			<!-- Hero Slider Navigation Right -->
+			<div class="hero_slider_nav hero_slider_nav_right">
+				<div class="hero_slider_next d-flex flex-column align-items-center justify-content-center trans_200">
+					<i class="fas fa-chevron-right trans_200"></i>
+				</div>
+			</div>
+
+		</div>
+
+	
+		<div class="next_section_scroll">
+			<div class="next_section nav_links" data-scroll-to=".icon_boxes">
+				<i class="fas fa-chevron-down trans_200"></i>
+				<i class="fas fa-chevron-down trans_200"></i>
+			</div>
+		</div>
+			
+	</div>
+
+	<!-- Footer -->
+
+	<footer class="footer">
+		<div class="container">
+			<div class="row">
+				
+				<div class="col-lg-4">
+
+					<!-- Footer Intro -->
+					<div class="footer_intro">
+
+						<!-- Logo -->
+						<div class="logo footer_logo">
+							<a href="#">Tadesa</span></a>
+						</div>
+
+						<p>Talleres deportivos TADESA por un mejor El Salvador.</p>
+						
+						<!-- Social -->
+						<div class="footer_social">
+							<ul>
+								<li><a href="https://www.facebook.com/Talleres-Deportivos-Salvadore%C3%B1os-Tadesa-183098068375455/"><i class="fab fa-facebook-f"></i></a></li>
+								<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+							</ul>
+						</div>
+						
+						<!-- Copyright -->
+						<div class="footer_cr">
+						Copyright &copy;<script>document.write(new Date().getFullYear());</script> Todos los derechos reservados</a>
+						</div>
+
+					</div>
+
+				</div>
+				
+				<!-- Footer Menu -->
+				<div class="col-lg-2">
+				<div class="footer_col"></div>
+				</div>
+				<div class="col-lg-2">
+				<div class="footer_col"></div>
+				</div>
+				<div class="col-lg-2">
+				<div class="footer_col"></div>
+				</div>
+
+				<div class="col-lg-2">
+
+					<div class="footer_col">
+						<div class="footer_col_title">Menu</div>
+						<ul>
+						<li class="active"><a href="#">Inicio</a></li>
+						<li><a href="about.php">Quiénes somos</a></li>
+						<li><a href="places.php">Instalaciones</a></li>								
+						<li><a href="contributions.php">Donaciones</a></li>
+						<li><a href="contact.php">Contáctanos</a></li>
+						</ul>
+					</div>
+	</footer>
+
+</div>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="styles/bootstrap4/popper.js"></script>
+<script src="styles/bootstrap4/bootstrap.min.js"></script>
+<script src="plugins/greensock/TweenMax.min.js"></script>
+<script src="plugins/greensock/TimelineMax.min.js"></script>
+<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="plugins/greensock/animation.gsap.min.js"></script>
+<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="plugins/slick-1.8.0/slick.js"></script>
+<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="plugins/scrollTo/jquery.scrollTo.min.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="js/custom.js"></script>
+</body>
+
+</html>
